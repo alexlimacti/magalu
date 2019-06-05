c7c93f4e5a2c266eff1f91e0e877059faea203b1
Alexsandro Lima <alexlima_cti@hotmail.com>
2019-06-05 11:05:40 -0300

    implementacao do servico - implementacao dos metodos de impressao

--- src/main/java/com/magalu/desafio/service/Services.java
+++ src/main/java/com/magalu/desafio/service/Services.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -16,7 +17,9 @@ import org.springframework.stereotype.Service;
 
 import com.magalu.desafio.domain.Game;
 import com.magalu.desafio.domain.Player;
+import com.magalu.desafio.domain.PlayerDeath;
 import com.magalu.desafio.domain.PlayerKill;
+import com.magalu.desafio.enumerator.Death;
 
 @Service
 public class Services {
@@ -39,18 +42,26 @@ public class Services {
 	}
 	
 	
-	
-	private String quotes(final String string) {
-		return "\"" + string + "\"";
+	//Impressao
+	@SuppressWarnings("unused")
+	private void nomeGame(final Game game) {
+		System.out.println(game.getName() + ": {");
 	}
 	
-	private List<PlayerKill> killByPlayer(final List<Game> games) {
+	private String quotes(String string) {
+		    if (!string.isEmpty()) {
+		        string = "\"" + string + "\"";
+		    }
+		return string;
+	}
+	
+	private List<PlayerKill> killPorPlayer(final List<Game> games) {
 		return games.stream().map(Game::getPlayerKills).flatMap(List::stream).collect(toList());
 	}
 	
 	private final Map<String, Integer> agruparKillPorPlayer(final List<Game> games) {
 		final Map<String, Integer> killsByNickName = new HashMap<>();
-		final List<PlayerKill> playerKills = killByPlayer(games);
+		final List<PlayerKill> playerKills = killPorPlayer(games);
 		for (final PlayerKill playerKill : playerKills) {
 			final String playerNickName = playerKill.getPlayer().getNickName();
 			final Integer count = playerKill.getCount();
@@ -64,15 +75,61 @@ public class Services {
 	}
 	
 	@SuppressWarnings("unused")
+	private Map<Death, Integer> agruparPorDeath(final Game game) {
+
+		final List<PlayerDeath> playerDeaths = game.getPlayerDeaths();
+		final Map<Death, Integer> contarPorDeath = new HashMap<>();
+
+		for (final PlayerDeath playerDeath : playerDeaths) {
+			final Map<Death, Integer> playerCountByDeaths = playerDeath.getContarPorDeath();
+
+			for (final Entry<Death, Integer> entry : playerCountByDeaths.entrySet()) {
+				final Death death = entry.getKey();
+				final Integer count = entry.getValue();
+
+				if (!contarPorDeath.containsKey(death)) {
+					contarPorDeath.put(death, 0);
+				}
+
+				final Integer previous = contarPorDeath.get(death);
+				contarPorDeath.put(death, previous + count);
+			}
+		}
+
+		return contarPorDeath;
+	}
+	
+	@SuppressWarnings("unused")
 	private void overall(final List<Game> games) {
 		System.out.println("Overall: {");
-		final Map<String, Integer> killsByName = agruparKillPorPlayer(games);
-		for (final Entry<String, Integer> entry : killsByName.entrySet()) {
+		final Map<String, Integer> killsPorNickName = agruparKillPorPlayer(games);
+		for (final Entry<String, Integer> entry : killsPorNickName.entrySet()) {
 			final String nickName = entry.getKey();
 			final Integer kills = entry.getValue();
 			System.out.println(ESPACO + "\"" + nickName + "\": " + kills);
 		}
 		System.out.println("}");
+	}
+	
+	@SuppressWarnings("unused")
+	private void kills(final Game game) {
+		if (game.getPlayerKills().isEmpty()) {
+			return;
+		}
+		System.out.println(ESPACO + "Kills: {");
+		final Iterator<PlayerKill> iterator = game.getPlayerKills().iterator();
+		while (iterator.hasNext()) {
+			final PlayerKill playerKill = iterator.next();
+			final Player player = playerKill.getPlayer();
+			System.out.print(ESPACO + ESPACO + "\"" + player.getNickName() + "\": " + playerKill.getCount());
+			if (iterator.hasNext()) {
+				System.out.println(",");
+
+			} else {
+				System.out.println();
+			}
+		}
+		System.out.println(ESPACO + "}");
 	}
 	
 	@SuppressWarnings("unused")
@@ -82,10 +139,42 @@ public class Services {
 		System.out.println(ESPACO + "total_kills: " + totalCount + ";");
 	}
 	
+	private void killsPorMeios(final Game game) {
+		if (game.getPlayerDeaths().isEmpty()) {
+			return;
+		}
+		System.out.println(ESPACO + "kills_por_meios: {");
+		final Map<Death, Integer> countByDeaths = agruparPorDeath(game);
+		final Iterator<Entry<Death, Integer>> iterator = countByDeaths.entrySet().iterator();
+		while (iterator.hasNext()) {
+			final Entry<Death, Integer> countPorDeath = iterator.next();
+			final Death death = countPorDeath.getKey();
+			final Integer count = countPorDeath.getValue();
+			System.out.print(ESPACO + ESPACO + "\"" + death + "\": " + count);
+			if (iterator.hasNext()) {
+				System.out.println(",");
+			} else {
+				System.out.println();
+			}
+		}
+		System.out.println(ESPACO + "}");
+	}
+	
 	@SuppressWarnings("unused")
 	private void players(final Game game) {
 		final String players = game.getPlayers().stream().map(Player::getNickName).map(this::quotes).collect(joining(", "));
-
 		System.out.println(ESPACO + "players: [" + players + "]");
+	}
+	
+	@SuppressWarnings("unused")
+	private void game(final List<Game> games) {
+		for (final Game game : games) {
+			nomeGame(game);
+			totalKills(game);
+			players(game);
+			kills(game);
+			killsPorMeios(game);
+			System.out.println("}");
+		}
 	}
 }
